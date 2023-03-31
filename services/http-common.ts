98e966253c94df3e2c46456cb3f070e8b6d6086f
RekhaDinesh <117529319+RekhaDinesh@users.noreply.github.com>
2023-03-31 23:59:03 +0530

    Update http-common.ts
    Commit changes for docker

--- services/http-common.ts
+++ services/http-common.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://localhost:3001/",
+    baseURL: "http://3.94.231.14:3001/",
     //baseURL: "https://roombookingappus-vftldv7jma-uc.a.run.app/",
 });
 export const ConnectionMgmtEndPoint = axios.create({
-    baseURL: "https://localhost:7177/",
+    baseURL: "http://3.94.231.14:3002/",
     //baseURL: "https://roombookingappus-vftldv7jma-uc.a.run.app/",
 });
 export const config = { headers: { "Content-type": "application/json" } };
