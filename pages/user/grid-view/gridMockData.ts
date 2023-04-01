02c0bb88e2041eba68ae2876f40e29cde4cfbc71
RekhaDinesh <117529319+RekhaDinesh@users.noreply.github.com>
2023-04-01 18:37:51 +0530

    UserManagement

---
+++ pages/user/grid-view/gridMockData.ts
@@ -0,0 +1,22 @@
+export const userListColumn = [
+    { field: 'Name', headerName: 'Name', width: 130 },
+    { field: 'Email', headerName: 'Email', width: 130 },
+    { field: 'Roles', headerName: 'Roles', width: 90 },
+    { field: 'Joined', headerName: 'Joined', width: 160 },
+    { field: 'Actions', headerName: 'Actions', width: 160 }
+]
+
+export const roleListColumn = [
+    { field: 'Role', headerName: 'Role', width: 130 },
+    { field: 'RoleBase', headerName: 'Role Base', width: 130 },
+    { field: 'Actions', headerName: 'Actions', width: 160 }
+]
+
+export const userListData = [
+    {
+        Name: "John Doe",
+        Email: "dinesh.indermull@gmail.com",
+        Roles :"admin",
+        Joined :"20/06/23"
+    }
+]
