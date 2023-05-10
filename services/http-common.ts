90908962c3a3074d41fa43e050518eddb044db23
Arun Alagesan <arun.alagesan@gmail.com>
2023-05-10 11:29:09 +0530

    updated the url to point aws

--- services/http-common.ts
+++ services/http-common.ts
@@ -1,19 +1,22 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://localhost:7177/",
+    baseURL: "http://3.94.231.14:3001/",
+    //baseURL: "https://localhost:7177/",
     // baseURL: "https://localhost:3001/",
     //baseURL: "https://roombookingappus-vftldv7jma-uc.a.run.app/",
 });
 export const ConnectionMgmtEndPoint = axios.create({
-    baseURL: "https://localhost:7177/",
+    baseURL: "http://3.94.231.14:3002/",
+    //baseURL: "https://localhost:7177/",
     //baseURL: "https://roombookingappus-vftldv7jma-uc.a.run.app/",
 });
 export const config = { headers: { "Content-type": "application/json" },
 
 connectionManagement:{
     //baseURL: "https://roombookingappus-vftldv7jma-uc.a.run.app/",
-    baseURL: "https://localhost:7022/",
+    baseURL: "http://3.94.231.14:3002/",
+    //baseURL: "https://localhost:7022/",
     AddCalender:"api/connector/addcalendar",
     CreateConnectionFromFile:"api/connector/createconnectionfromfile",
     GetAllConnectors:"api/connector/getall"
