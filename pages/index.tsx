311cee87147cbc0ca12d7b9a734a945248dd9e6d
arun.alagesan <arun.alagesan@gmail.com>
2023-07-24 19:15:16 +0530

    added forms for authetication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
   import Layout from "../components/Layout";
-import Sidebar from "../components/Sidebar";
+  import SignIn from "../components/auth/SignIn";
+
+//import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   // this component is not used
   return (
     <div className="p-0 fullHeight">
-      <Layout />
+      {/*<Layout /> */}
+      <SignIn />
     </div>
   )
 }
