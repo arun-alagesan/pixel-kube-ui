9b92eaed5e3c6e3d004759f0068d8583b5866aee
Arun Alagesan <arun.alagesan@gmail.com>
2023-08-02 22:31:54 +0530

    removed unwangted code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
-import Sidebar from "../components/Sidebar";
 
 export default function Home() {
+
   // this component is not used
   return (
     <div className="p-0 fullHeight">
@@ -9,3 +9,5 @@ export default function Home() {
     </div>
   )
 }
+
+
